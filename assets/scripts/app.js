61a3b22e3b0e2eaba6fc84556f7d355ba073ae86
victorclee <victorchristopherlee@gmail.com>
2020-01-02 15:53:19 -0600

    Setting up max life and attack.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,14 @@
-const chosenMaxLife = 100;
+const ATTACK_VALUE = 10;
+
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function attackHandler() {
+  const damage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth -= damage;
+}
+
+attackBtn.addEventListener('click', attackHandler);
