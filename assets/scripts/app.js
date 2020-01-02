5f9a32ddb4ee7f4c64f1af195dd872f771b3cb62
victorclee <victorchristopherlee@gmail.com>
2020-01-02 15:58:49 -0600

    Added player damage and monster damage.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -9,6 +10,11 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler() {
   const damage = dealMonsterDamage(ATTACK_VALUE);
   currentMonsterHealth -= damage;
+  const monsterDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= monsterDamage;
+  if (currentMonsterHealth <= 0) {
+    alert('You won!')
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
